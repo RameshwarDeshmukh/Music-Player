c4479dd7060dd58d859dd725e3818fdeb1cda804
RameshwarDeshmukh <123760947+RameshwarDeshmukh@users.noreply.github.com>
2023-01-27 23:19:41 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,80 @@
+let allMusic = [
+  {
+    name: "Itni Si Baat Hain",
+    artist: "Arijit Singh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Jo Tu Na Mila",
+    artist: "Asim Azhar",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Kahani",
+    artist: "Pritam, Mohan",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Lo Maan Liya",
+    artist: "Arijit Singh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Main Rahoon Ya Na Rahoon",
+    artist: "Aramn Malik",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Muskurane Ki Wajah Tum Ho",
+    artist: "Arijit Singh",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Tera Chehra",
+    artist: "Arijit Singh",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Naino Ne Baandhi (Gold)",
+    artist: "Yasser Desai",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Rozana",
+    artist: "Shreya Ghoshal",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Ghode Pe Sawar",
+    artist: "Sireesha Bhagavatula",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Phir Na Aisi Raat Aayegi",
+    artist: "Arijit Singh",
+    img: "music-7",
+    src: "music-11"
+  },
+  {
+    name: "Zindagi Ne Pehni Hai Muskan",
+    artist: "Roop Kumar Rathod",
+    img: "music-10",
+    src: "music-12"
+  },
+  {
+    name: "Rangdaari",
+    artist: "Arijit Sing",
+    img: "music-10",
+    src: "music-13"
+  },
+];
